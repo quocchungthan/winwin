49e6f0f3e8826673c008f232fbb17bb42897de39
cbt <quocchung.than@gmail.com>
2025-10-01 14:46:59 +0700

    code for dayone tape 1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Hello from TikTok Bầu Cua Tôm Cá!</h1>
+      <p>This is the skeleton app. To test the Netlify function, visit <code>/.netlify/functions/hello</code> after deploying.</p>
+    </main>
+  )
+}
+
+export default App
